169e82bd0f4b8489cc0053e7cdf0d5ed6e2a70f0
Natalie Nicole <natalienicole@MacBook-Pro.local>
2023-07-22 20:03:32 -0400

    add randomly generated json, create connection

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+mongoose.set("returnOriginal", false);
+
+mongoose.connect("mongodb://127.0.0.1:27017/unit2API")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => {
+    console.log(`Error connecting to MongoDB: ${err.message}`);
+  });
+
+mongoose.connection.on("disconnected", () => {
+  console.log(chalk.bold("Disconnected from MongoDB"));
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(chalk.red(`MongoDB connection error: ${err}`));
+});
+
+export default mongoose.connection;
